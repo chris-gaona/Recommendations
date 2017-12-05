4bde486564a840f84b32894ef19790fb3e6662ae
Chris Gaona <thebeloved88@sbcglobal.net>
2017-12-05 09:06:39 -0800

    Adds more to UI to get everything wired up

--- app/src/main/java/com/chrisgaona/recommendations/MainActivity.java
+++ app/src/main/java/com/chrisgaona/recommendations/MainActivity.java
@@ -2,12 +2,44 @@ package com.chrisgaona.recommendations;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private View mProgressBar;
+    private TextView mErrorView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = findViewById(R.id.recyclerview);
+        mProgressBar = findViewById(R.id.progressbar);
+        mErrorView = findViewById(R.id.error_view);
+
+        showLoading();
+    }
+
+    public void showLoading() {
+        mProgressBar.setVisibility(View.VISIBLE);
+        mRecyclerView.setVisibility(View.GONE);
+        mErrorView.setVisibility(View.GONE);
+    }
+
+    public void showList() {
+        mProgressBar.setVisibility(View.GONE);
+        mRecyclerView.setVisibility(View.VISIBLE);
+        mErrorView.setVisibility(View.GONE);
+    }
+
+    public void showError() {
+        mProgressBar.setVisibility(View.GONE);
+        mRecyclerView.setVisibility(View.GONE);
+        mErrorView.setVisibility(View.VISIBLE);
     }
 }
